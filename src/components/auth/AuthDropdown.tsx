2b6b6ad95cd74e4cea46493c7a4a8154ad325c92
Alvarogfs <alvarogomesalferes@gmail.com>
2023-10-30 19:21:19 +0100

    text white dropdown

--- src/components/auth/AuthDropdown.tsx
+++ src/components/auth/AuthDropdown.tsx
@@ -24,24 +24,24 @@ const AuthDropdown:FC<{
 
   >
     <Dropdown.Header>
-      <span className="block text-sm">
+      <span className="block text-sm text-white">
         {name}
       </span>
-      <span className="block truncate text-sm font-medium">
+      <span className="block truncate text-sm font-medium text-white">
         {email}
       </span>
     </Dropdown.Header>
-    <Dropdown.Item className='dark:hover:text-black hover:text-black'>
+    <Dropdown.Item className='dark:hover:text-black text-white hover:text-black'>
       Dashboard
     </Dropdown.Item>
-    <Dropdown.Item className='dark:hover:text-black hover:text-black'>
+    <Dropdown.Item className='dark:hover:text-black text-white hover:text-black'>
       Settings
     </Dropdown.Item>
-    <Dropdown.Item className='dark:hover:text-black hover:text-black'>
+    <Dropdown.Item className='dark:hover:text-black text-white hover:text-black'>
       Earnings
     </Dropdown.Item>
     <Dropdown.Divider />
-    <Dropdown.Item className='dark:hover:text-black hover:text-black' onClick={() => handleSignOut()}>
+    <Dropdown.Item className='dark:hover:text-black text-white hover:text-black' onClick={() => handleSignOut()}>
       Sign out
     </Dropdown.Item>
   </Dropdown>
